61248c42041f9dddd138d926ebc14acf88beef9d
han0110 <tinghan0110@gmail.com>
2019-12-27 11:53:09 +0800

    :boom: Upgrade to traefik v2

--- script/docker-compose-alias.sh
+++
@@ -1,5 +0,0 @@
-alias docker-compose='docker run --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$PWD:$PWD" \
-    -w="$PWD" \
-    docker/compose:1.13.0'
--- script/start.sh
+++ script/start.sh
@@ -5,18 +5,17 @@ ENV_DEFAULT_FILE=".env.default"
 function main() {
     ENV_FILE=$1
     # Use default env file if not specified
-    if [ -f "${ENV_FILE}" ]; then
-        source ${ENV_FILE}
-    else
-        source ${ENV_DEFAULT_FILE}
+    if [ ! -f "${ENV_FILE}" ]; then
+        ENV_FILE=${ENV_DEFAULT_FILE}
     fi
+    source ${ENV_FILE}
     # Create docker network
     docker network create $REVERSE_PROXY_NETWORK
     docker network create $SERVICES_NETWORK
     # Pull images
     docker-compose pull
     # Start proxy
-    docker-compose up -d
+    env `cat ${ENV_FILE} | grep -v '^#'` docker-compose up -d
     exit 0
 }
 
