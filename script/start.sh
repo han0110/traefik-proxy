cccdb877e120447553eec25aa7e4b59c08941d71
han0110 <tinghan0110@gmail.com>
2020-02-07 08:35:49 +0800

    :wrench: Simplify start script and docker-compose

--- script/start.sh
+++ script/start.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
 ENV_DEFAULT_FILE=".env.default"
+ENV_FILE=".env"
+
+REVERSE_PROXY_NETWORK="reverse-proxy"
+
+function use_env_if_exist() {
+    if [ -f "$1" ]; then
+        echo "Use env file $1"
+        source $1
+    fi
+}
 
 function main() {
-    ENV_FILE=$1
-    # Use default env file if not specified
-    if [ ! -f "${ENV_FILE}" ]; then
-        ENV_FILE=${ENV_DEFAULT_FILE}
-    fi
-    source ${ENV_FILE}
-    # Create docker network
-    docker network create $REVERSE_PROXY_NETWORK
-    docker network create $SERVICES_NETWORK
-    # Pull images
-    docker-compose pull
-    # Start proxy
-    env `cat ${ENV_FILE} | grep -v '^#'` docker-compose up -d
+    # Source environment files
+    use_env_if_exist ${ENV_DEFAULT_FILE}
+    use_env_if_exist ${ENV_FILE}
+    # Create docker network for traefik and services
+    docker network create ${REVERSE_PROXY_NETWORK} 2>/dev/null 
+    # Start traefik
+    docker-compose up -d --force-recreate
     exit 0
 }
 
